79836585dcd84878c8272adc6cdc302fe38d491a
hyEOnjuNg0-0 <117015040+hyEOnjuNg0-0@users.noreply.github.com>
2025-10-08 13:07:22 +0900

    Add files via upload

--- merge_live_product_code.py
+++ merge_live_product_code.py
@@ -1,34 +1,39 @@
 import pandas as pd
 
-brand = 'L'  # 'B', 'D', 'L' *********************************************************************************************************************************
+brand = 'B'  # 'B', 'D', 'L' *********************************************************************************************************************************
 
 # CSV 불러오기
-comments_df = pd.read_csv(rf"D:\School\5-2\데이터\shoppinglive_data\실시간 댓글\{brand}_Live_Comment.csv")
+df = pd.read_csv(rf"D:\School\5-2\{brand}_liveinfo.csv")
 codes_df = pd.read_csv(rf"D:\School\5-2\데이터\shoppinglive_data\live_code_{brand}.csv")
 
-# 기존 comments_df 행 수 확인
-print("Number of rows before merge : ", len(comments_df))
+# 기존 df 행 수 확인
+print("Number of rows before merge : ", len(df))
 
-# comments_df 컬럼명 변경
-comments_df = comments_df.rename(columns={"video_url": "live_url"})
+# df 컬럼명 변경
+df = df.rename(columns={"url": "live_url"})
 
-# live_url 앞의 불필요한 부분 제거
-comments_df["live_url"] = comments_df["live_url"].str.replace(rf"^{brand}_", "", regex=True)
+# live_url 앞의 불필요한 부분 제거 (존재할 때만)
+df["live_url"] = df["live_url"].str.replace(rf"^{brand}_", "", regex=True)
+
+# /replays/ 뒤의 숫자만 추출해서 새로운 컬럼 생성
+df["url_code"] = df["live_url"].str.extract(r'/replays/(\d+)')
+codes_df["url_code"] = codes_df["live_url"].str.extract(r'/replays/(\d+)')
 
 # codes_df 에서 필요한 컬럼만 선택
-codes_df = codes_df[["live_url", "live_code"]]
+codes_df = codes_df[["url_code", "live_code"]]
 
-# comments_df 기준 병합
-merged_df = pd.merge(comments_df, codes_df, on="live_url", how="left")
+# df 기준 병합
+merged_df = pd.merge(df, codes_df, on="url_code", how="left")
 
 # 원하는 컬럼 순서 지정
-desired_order = ["live_code", "time", "nickname", "comment", "live_url"]
+# desired_order = ["live_code", "time", "nickname", "comment", "total_duration(sec)","live_url"]
+desired_order = ["live_code", "date", "start_time", "end_time", "duration_min","viewer_count", "promotion_flag", "promotion_text"]
 merged_df = merged_df[desired_order]
 
 # 병합 후 merged_df 행 수 확인
 print("Number of rows after merge : ", len(merged_df))
 
 # 결과 저장
-merged_df.to_csv(rf"D:\School\5-2\데이터\shoppinglive_data\live_comment_{brand}.csv", index=False, encoding="utf-8-sig")
+merged_df.to_csv(rf"D:\School\5-2\데이터\shoppinglive_data\live_info_{brand}.csv", index=False, encoding="utf-8-sig")
 
 print(merged_df.head(10))
